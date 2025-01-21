05bfa530e6fb555a2aca4e999a9be35055182091
Kevin Palomino <kevinpalomino117@gmail.com>
2025-01-20 21:36:26 -0500

    Agregando mensaje en el boton prompt

--- js-curso-2/app.js
+++ js-curso-2/app.js
@@ -4,3 +4,8 @@ titulo.innerHTML = 'Hora del Desafío';
 function consoleButton(){
     alert('El boton fue clickeado');
 }
+
+function preguntaCiudad(){
+    let ciudad = prompt("Mencioname un nombre de una ciudad de Brasil");
+    alert(`Estuve en ${ciudad} y me acorde de ti`);
+}
