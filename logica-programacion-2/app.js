53bc9ee1b146539c4261027659c08bf2e387d148
Kevin Palomino <kevinpalomino117@gmail.com>
2025-01-20 19:53:52 -0500

    Interaccion javaScript-HTML

---
+++ logica-programacion-2/app.js
@@ -0,0 +1,6 @@
+let titulo = document.querySelector('h1');
+//innerHTML para poner el titulo de tipo string
+titulo.innerHTML = 'Juego del número secreto';
+
+let parrafo = document.querySelector('p');
+parrafo.innerHTML = 'Indica un número del 1 al 10'
