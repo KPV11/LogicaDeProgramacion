778ebc763a63af143e32bcd3a545a04c1c1f4bf4
Kevin Palomino <kevinpalomino117@gmail.com>
2025-01-20 20:30:35 -0500

    Funcion Button

--- logica-programacion-2/app.js
+++ logica-programacion-2/app.js
@@ -4,3 +4,7 @@ titulo.innerHTML = 'Juego del número secreto';
 
 let parrafo = document.querySelector('p');
 parrafo.innerHTML = 'Indica un número del 1 al 10'
+
+function intentoDeUsuario(){
+    alert('Click desde el boton');
+}
