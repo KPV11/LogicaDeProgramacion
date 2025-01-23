42c7391e5c21d72ea582e9d3ab9e80dbfb0ac098
Kevin Palomino <kevinpalomino117@gmail.com>
2025-01-22 19:43:40 -0500

    Actualizacion del index

--- logica-programacion-2/app.js
+++ logica-programacion-2/app.js
@@ -1,10 +1,27 @@
-let titulo = document.querySelector('h1');
-//innerHTML para poner el titulo de tipo string
-titulo.innerHTML = 'Juego del número secreto';
+let numeroSecreto = generarNumeroSecreto();
 
-let parrafo = document.querySelector('p');
-parrafo.innerHTML = 'Indica un número del 1 al 10'
+function asignarTextoElemento(elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    //innerHTML para poner el titulo de tipo string
+    elementoHTML.innerHTML = texto;
+    
+}
 
-function intentoDeUsuario(){
-    alert('Click desde el boton');
+function verificarIntento(){
+    let numeroUsuario = parseInt(document.getElementById('valorUsuario').value);
+    console.log(typeof(numeroUsuario));
+    console.log(numeroSecreto);
+    console.log(typeof(numeroSecreto));
+    console.log(numeroUsuario);
+    // el === compara no solo el valor sino tambien el tipo de dato sea igual
+    console.log(numeroUsuario === numeroSecreto);
+    return;
+}
+
+asignarTextoElemento('h1','Juego del número secreto!');
+asignarTextoElemento('p','Indica un número del 1 al 100');
+
+function generarNumeroSecreto() {
+    return Math.floor(Math.random()*10)+1;
+    
 }
