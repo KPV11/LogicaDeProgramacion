794287efc77ced14aac96fcf8a6fa39ac8afca9b
Kevin Palomino <kevinpalomino117@gmail.com>
2025-01-22 19:47:45 -0500

    Desafio2 resuelto

---
+++ desafios/desafio1.js
@@ -0,0 +1,57 @@
+/*
+1. Crear una función que muestre "¡Hola, mundo!" en la consola
+function saludar(){
+    console.log("Hola, mundo");
+}
+saludar();
+*/
+/*
+2. Crear una función que reciba un nombre como parámetro y muestre "¡Hola, [nombre]!" en la consola.
+function saludar(nombre){
+    console.log(`Hola ${nombre}`);
+}
+saludar('Kevin');
+*/
+/*
+3. Crear una función que reciba un número como parámetro y devuelva el doble de ese número.
+function dobleNumero(numero) {
+    doble = 2*numero;
+    console.log(`El doble del numero ${numero} es: ${doble} `);
+    return doble;
+}
+dobleNumero(5);
+*/
+/*
+4. Crear una función que reciba tres números como parámetros y devuelva su promedio.
+function promedioNumeros(x1,x2,x3) {
+    promedio = (x1+x2+x3)/3;
+    console.log(`El medio de los numeros ${x1}, ${x2}, ${x3} es: ${promedio} `);
+    return promedio;    
+}
+promedioNumeros(8,7,5);
+*/
+
+/*
+5. Crear una función que reciba dos números como parámetros y devuelva el mayor de ellos.
+function numeroMayor(x1,x2) {
+    if(x1>x2){
+        console.log(x1);
+    }
+    else if(x1<x2){
+        console.log(x2);
+    }
+    else{
+        console.log("Los números son iguales");
+    }
+}
+numeroMayor(5,5);
+*/
+
+/*
+6.Crear una función que reciba un número como parámetro y devuelva el resultado de multiplicar ese número por sí mismo.
+function multiplicarMismoNumero(numero) {
+    multpicar = numero * numero;
+    console.log(multpicar);
+}
+multiplicarMismoNumero(6);
+*/
